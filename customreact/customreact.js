a1297ebe71d527655ced1dac431f9f307c4d7a82
Abhinav Kartik <abhinavkartik2026@gmail.com>
2026-04-05 10:16:33 +0530

    custom

---
+++ customreact/customreact.js
@@ -0,0 +1,28 @@
+function customrender(reactelement, container) {
+    // const domelement = document.createElement(reactelement.type)
+    // domelement.innerHTML = reactelement.childeren
+    // domelement.setAttribute('href', reactelement.props.href)
+    // domelement.setAttribute('target', reactelement.props.target)
+    // container.appendChild(domelement)
+    const domelement = document.createElement(reactelement.type)
+    domelement.innerHTML = reactelement.children
+    for (const prop in reactelement.props) {
+        if (prop === "children") continue;
+        domelement.setAttribute(prop, reactelement.props
+        [prop]
+        )
+    }
+    container.appendChild(domelement)
+}
+
+const reactelement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'click me to visit google'
+}
+const maincontainer = document.querySelector('#root')
+
+customrender(reactelement, maincontainer)
